67dc85c74aba54da54bdac96624764e4e0e266a1
Mas Bowo <C14220311@john.petra.ac.id>
2026-06-21 14:42:01 +0700

    feat: add start.sh to quickly run the program in linux/macos

---
+++ Laravel/public/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Hardcoded to your exact Laravel root directory
+DIR="/Users/satriyahandhawibowo/Documents/Skripsi-C14220311/Laravel"
+
+echo "Launching Skripsi services from: $DIR"
+
+# Window 1: PHP Artisan Serve
+osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && php artisan serve\""
+
+# Window 2: PHP Artisan Queue
+osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && php artisan queue:work\""
+
+# Window 3: Python API Server
+osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && python3 python_scripts/api_server.py\""
