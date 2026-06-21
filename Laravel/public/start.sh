7ca5673dc708e20b651086bc1ff4940ba91612e2
Mas Bowo <C14220311@john.petra.ac.id>
2026-06-21 21:44:59 +0700

    feat:improve status label color in sales and purchase page

--- Laravel/public/start.sh
+++ Laravel/public/start.sh
@@ -11,6 +11,5 @@ osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && php arti
 # Window 2: PHP Artisan Queue
 osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && php artisan queue:work --timeout=3700 --tries=1\""
 
-
 # Window 3: Python API Server
 osascript -e "tell application \"Terminal\" to do script \"cd '$DIR' && python3 python_scripts/api_server.py\""
